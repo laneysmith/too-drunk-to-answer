ceefb18c01291d91af095b541d05f58e1980b863
Laney Smith <lsmith.unc@gmail.com>
2016-07-29 13:49:21 -0600

    Add random answer generator

--- js/data.js
+++ js/data.js
@@ -1,14 +1,38 @@
 var answers = [
     {
-        "id": 1,
         "asker": "mom",
         "question": "blah",
-        "reponse": "asdfasdfasdf"
+        "response": "abc"
     },
     {
-        "id": 2,
-        "asker": "gf/bf",
-        "question": "stuff",
-        "reponse": "asdfafdgagssd"
+        "asker": "mom",
+        "question": "blah",
+        "response": "def"
+    },
+    {
+        "asker": "girlfriend/boyfriend",
+        "question": "Where are you?",
+        "response": "I'm at a Java meet-up. I'll be home soon."
+    },
+    {
+        "asker": "mom",
+        "question": "Where are you?",
+        "response": "Can't say where I am, but I can tell you where I'm not."
+    },
+    {
+        "asker": "cop",
+        "question": "Where are heading?",
+        "response": "Looking for the fella who stole my shoes."
+    },
+    {
+        "asker": "cop",
+        "question": "Do you know why I pulled you over?",
+        "response": "Wouldn't have anything to do with me driving 7mph on the sidewalk, would it?"
+    },
+    {
+        "asker": "girlfriend/boyfriend",
+        "question": "Why are you ignoring my texts?",
+        "response": ""
     }
+
 ]
